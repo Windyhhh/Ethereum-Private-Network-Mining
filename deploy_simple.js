a48b115209936964b76f215e5dcb0a32d1b2f603
笙囧同学 <92248340+Windyhhh@users.noreply.github.com>
2026-08-23 17:38:37 +0800

    Add deploy_simple.js

---
+++ deploy_simple.js
@@ -0,0 +1,23 @@
+// 简化版ERC20部署脚本
+// 使用方法：
+// 1. 复制ZTSCoin.sol到Remix
+// 2. 编译合约
+// 3. 部署到http://localhost:8545
+
+console.log("=== 简化版ERC20部署指南 ===");
+console.log("1. 打开 Remix: https://remix.ethereum.org/");
+console.log("2. 上传 ZTSCoin.sol 文件");
+console.log("3. 点击 'Compile' 标签编译合约");
+console.log("4. 点击 'Deploy & Run Transactions' 标签");
+console.log("5. 环境选择 'Web3 Provider'");
+console.log("6. 输入连接地址: http://localhost:8545");
+console.log("7. 在 'Deploy' 部分选择 ZTSCoin 合约");
+console.log("8. 点击 'Deploy' 按钮");
+console.log("9. 等待交易确认");
+console.log("10. 测试合约功能");
+console.log("\n=== 测试命令 ===");
+console.log("- name(): 查看代币名称");
+console.log("- symbol(): 查看代币符号");
+console.log("- totalSupply(): 查看总供应量");
+console.log("- balanceOf(address): 查看账户余额");
+console.log("- transfer(address, amount): 转账代币");
